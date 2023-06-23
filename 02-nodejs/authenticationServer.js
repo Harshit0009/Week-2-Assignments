df8815f5fdec934744ca846885543dda1d013542
Harshit Tiwari <tiwariharshit909@gmail.com>
2023-06-23 20:58:36 +0530

    done authentication, some test failed due to difference in output of me and test

--- 02-nodejs/authenticationServer.js
+++ 02-nodejs/authenticationServer.js
@@ -29,9 +29,126 @@
   Testing the server - run `npm run test-authenticationServer` command in terminal
  */
 
-const express = require("express")
+const express = require("express");
+const uuid = require("uuid");
 const PORT = 3000;
 const app = express();
 // write your logic here, DONT WRITE app.listen(3000) when you're running tests, the tests will automatically start the server
+app.use(express.json()); // parses json payload
+var logs = [];
+// creating signup functionality
+function handleSignUp(req, res) {
+  const uniqueId = uuid.v4();
+  var { username, password, firstname, lastname } = req.body;
+  console.log(`${username} ${password}`);
+  let user = {
+    uid: uniqueId,
+    username: username,
+    password: password,
+    firstname: firstname,
+    lastname: lastname,
+  };
+  // console.log(`user: ${user}`);
+  let userExists = false;
+  for (let i = 0; i < logs.length; i++) {
+    if (logs[i].username === user.username) {
+      userExists = true;
+      break;
+    }
+  }
+  if (userExists) {
+    return res.status(400);
+  } else {
+    logs.push(user);
+    // console.log(logs);
+    return res.status(201).send("Signup successful");
+  }
+}
 
+// creating signin functoinality
+function handleSignIn(req, res) {
+  let { username, password } = req.body;
+  let checked = false;
+  let index = -1;
+  for (let i = 0; i < logs.length; i++) {
+    if (logs[i].username === username) {
+      if (logs[i].password === password) {
+        checked = true;
+        index = i;
+        break;
+      } else {
+        return res.status(401);
+      }
+    }
+  }
+  console.log(index);
+  if (checked) {
+    return res.status(200).json({
+      success: true,
+      uuid: logs[index].uuid,
+      firstname: logs[index].firstname,
+      lastname: logs[index].lastname,
+      username: username,
+    });
+  } else {
+    return res
+      .status(401);
+  }
+}
+
+function handleGetData(req, res) {
+  let username = req.headers.username;
+  let password = req.headers.password;
+  // console.log(username);
+  // console.log(password);
+  let checked = false;
+  // let index = -1;
+  for (let i = 0; i < logs.length; i++) {
+    if (logs[i].username === username) {
+      if (logs[i].password === password) {
+        checked = true;
+        // index = i;
+        console.log(i);
+        break;
+      } else {
+        return res.status(401);
+      }
+    }
+  }
+  console.log(checked);
+  if (checked) {
+    // console.log("inside if block");
+    let returnArray = [];
+    for (let j = 0; j < logs.length; j++) {
+      returnArray.push({
+        firstname: logs[j].firstname,
+        lastname: logs[j].lastname,
+        username: logs[j].username,
+      });
+    }
+    // console.log(returnArray);
+    return res.status(200).json({ returnArray });
+  } else {
+    return res.status(401);
+  }
+}
+
+app.post("/signup", handleSignUp);
+app.post("/login", handleSignIn);
+app.get("/data", handleGetData);
+// app.get("", (req, res) => {
+//   return res.status(404).send("No routes found");
+// });
+// app.post("", (req, res) => {
+//   return res.status(404).send(`No routes found`);
+// });
+
+function serverFunc(err) {
+  if (err) {
+    console.log("error: " + err);
+  }
+  console.log(`app is running on port 3000.`);
+}
+
+// app.listen(PORT, serverFunc);
 module.exports = app;
